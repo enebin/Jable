df0114eae565d8ecd1f954231f95be56372f3c39
enebin <lyb35548@gmail.com>
2022-06-07 15:24:46 +0900

    Add files

---
+++ YellowIsTheNewBlack/MainTabViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  YellowIsTheNewBlack
+//
+//  Created by 이영빈 on 2022/05/29.
+//
+
+import UIKit
+import Then
+
+class MainTabViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        tabBar.tintColor = .yellow
+        tabBar.unselectedItemTintColor = .white
+        
+        let firstVC = RecordingViewController()
+        firstVC.tabBarItem = UITabBarItem(title: "녹화", image: UIImage(systemName: "record.circle"), tag: 0)
+        let secondVC = GalleryViewController()
+        secondVC.tabBarItem = UITabBarItem(title: "앨범", image: UIImage(systemName: "rectangle.stack"), tag: 1)
+        let thirdVC = SettingViewController()
+        thirdVC.tabBarItem = UITabBarItem(title: "세팅", image: UIImage(systemName: "gear"), tag: 2)
+        
+        var tabs = [firstVC, secondVC, thirdVC]
+        tabs = tabs.map { vc in
+            UINavigationController(rootViewController: vc)
+        }
+        
+        self.setViewControllers(tabs, animated: false)
+    }
+}
+
---
+++ YellowIsTheNewBlack/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryViewController.swift
+//  YellowIsTheNewBlack
+//
+//  Created by 이영빈 on 2022/05/29.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+}
