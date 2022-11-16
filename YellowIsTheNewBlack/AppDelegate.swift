e4ae2d1d162222f1f6082cef0a818736f542127b
Young Bin Lee <enebin777@gmail.com>
2022-11-16 15:56:49 +0900

    [Feature] Add review

--- YellowIsTheNewBlack/AppDelegate.swift
+++ YellowIsTheNewBlack/AppDelegate.swift
@@ -6,19 +6,12 @@
 //
 
 import UIKit
+import StoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        // Sets background to a blank/empty image
-//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        // Sets shadow (line below the bar) to a blank image
-//        UINavigationBar.appearance().shadowImage = UIImage()
-        // Sets the translucent background color
-//        UINavigationBar.appearance().backgroundColor = .black
-        // Set translucent. (Default value is already true, so this can be removed if desired.)
-//        UINavigationBar.appearance().isTranslucent = true
+        
         return true
     }
 }
--- YellowIsTheNewBlack/SceneDelegate.swift
+++ YellowIsTheNewBlack/SceneDelegate.swift
@@ -6,21 +6,39 @@
 //
 
 import UIKit
+import StoreKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         
         window?.overrideUserInterfaceStyle = .light // Disable darkmode
         window?.windowScene = windowScene
         window?.rootViewController = VideoRecorderViewController()
         window?.makeKeyAndVisible()
+        
+        let isAppOpenedCount = appOpenedCount(true)
+        if isAppOpenedCount == 2 || isAppOpenedCount == 5 || isAppOpenedCount == 8 {
+            SKStoreReviewController.requestReview()
+            print("@@", isAppOpenedCount)
+        }
+        
+    }
+}
+
+extension SceneDelegate {
+    func appOpenedCount(_ add: Bool) -> Int {
+        let count = UserDefaults.standard.integer(forKey: "isAppOpenedCount")
+        if add {
+            UserDefaults.standard.set(count + 1, forKey: "isAppOpenedCount")
+        }
+        
+        return count
     }
 }
 
